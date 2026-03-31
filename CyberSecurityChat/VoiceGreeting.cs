a294854c22d7c117d5fb21fd442ff58a4cea5139
Taariq Flint <st10470745@vcconnect.edu.za>
2026-03-31 20:54:10 +0200

    Implemented voice greeting using WAV file

---
+++ CyberSecurityChat/VoiceGreeting.cs
@@ -0,0 +1,20 @@
+﻿using System.Media;
+
+namespace CyberSecurityChatbot
+{
+    class VoiceGreeting
+    {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("audio/greeting.wav");
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Voice greeting could not be played.");
+            }
+        }
+    }
+}
